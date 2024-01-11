5174eaffd9997440ee629b12ea4823dbff07ed05
Simone Ballard <simone.a.ballard@gmail.com>
2024-01-10 22:15:00 -0600

    web config

---
+++ src/main/java/com/educative/ecommerce/config/WebConfig.java
@@ -0,0 +1,15 @@
+@Configuration
+class Webconfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedMethods("GET", "PUT", "POST", "PATCH", "DELETE", "OPTIONS");
+            }
+        };
+    }
+}
