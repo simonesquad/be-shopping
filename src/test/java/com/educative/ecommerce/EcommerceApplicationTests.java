488ab57fb712f75ab2f57baf396e61cc6a602b74
Simone Ballard <simone.a.ballard@gmail.com>
2024-01-15 21:54:02 -0600

    added application test file

--- src/test/java/com/educative/ecommerce/EcommerceApplicationTests.java
+++ src/test/java/com/educative/ecommerce/EcommerceApplicationTests.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package com.educative.ecommerce;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class EcommerceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
