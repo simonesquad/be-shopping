8ba0582fe4f3c36e282e96774c15e7a02ea20e96
Simone Ballard <simone.a.ballard@gmail.com>
2024-01-15 21:45:41 -0600

    updating all product with category lol

--- src/main/java/com/educative/ecommerce/service/ProductService.java
+++ src/main/java/com/educative/ecommerce/service/ProductService.java
@@ -2,6 +2,7 @@ package com.educative.ecommerce.service;
 
 
 import com.educative.ecommerce.dto.product.ProductDto;
+import com.educative.ecommerce.model.Category;
 import com.educative.ecommerce.model.Product;
 import com.educative.ecommerce.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,8 +18,9 @@ public class ProductService {
     @Autowired
     private ProductRepository productRepository;
 
-    public static Product getProductFromDto(ProductDto productDto) {
+    public static Product getProductFromDto(ProductDto productDto, Category category) {
         Product product = new Product();
+        product.setCategory(category);
         product.setDescription(productDto.getDescription());
         product.setImageURL(productDto.getImageURL());
         product.setPrice(productDto.getPrice());
@@ -27,8 +29,8 @@ public class ProductService {
     }
 
 
-    public void addProduct(ProductDto productDto) {
-        Product product = getProductFromDto(productDto);
+    public void addProduct(ProductDto productDto, Category category) {
+        Product product = getProductFromDto(productDto, category);
         productRepository.save(product);
     }
 
@@ -46,11 +48,15 @@ public class ProductService {
     }
 
     // update a product
-    public void updateProduct(Integer productID, ProductDto productDto) {
-        Product product = getProductFromDto(productDto);
+    public void updateProduct(Integer productID, ProductDto productDto, Category category) {
+        Product product = getProductFromDto(productDto, category);
         // set the id for updating
         product.setId(productID);
         // update
         productRepository.save(product);
     }
+
+
+
+
 }
